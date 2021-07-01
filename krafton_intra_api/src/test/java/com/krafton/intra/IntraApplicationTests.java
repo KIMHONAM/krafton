b46d704f790a06229a09fa51e42d31696519355d
KIM HONAM <khn228@naver.com>
2021-07-01 14:58:20 +0900

    웹/api 서버 작업 커밋

---
+++ krafton_intra_api/src/test/java/com/krafton/intra/IntraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krafton.intra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
