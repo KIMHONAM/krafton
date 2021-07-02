76fc67f1af6afa9ec13a73200686fc46c96c2cd7
KIM HONAM <khn228@naver.com>
2021-07-02 14:51:05 +0900

    에러 처리 기초 테스트

--- krafton_intra_api/src/main/java/com/krafton/intra/pto/controller/PTOController.java
+++ krafton_intra_api/src/main/java/com/krafton/intra/pto/controller/PTOController.java
@@ -1,5 +1,6 @@
 package com.krafton.intra.pto.controller;
 
+import com.krafton.intra.core.dto.ApiResponse;
 import com.krafton.intra.pto.service.PTOService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,26 @@ public class PTOController {
     PTOService ptoService;
 
     @GetMapping("/id/{id}")
-    public ResponseEntity<Object> getUserPtoInformation(@PathVariable int id){
+    public ResponseEntity<Object> getUserPtoInformation(@PathVariable int id) {
 
-        return ResponseEntity.ok(ptoService.getEmployeePTOInfo(id));
+        return ResponseEntity.ok(ApiResponse.builder().isSuccess(true).data(ptoService.getEmployeePTOInfo(id)).build());
     }
+
+    @GetMapping("/error/test")
+    public String testException() throws Exception {
+          if(1==1)  throw new Exception("111");
+        return "";
+    }
+
+    // 휴가 취소 리스트 조회 - get
+    // 휴가 취소 - post
+
+    // 휴가 신청 - post
+    // 휴가 일정 보기 - get
+
+    // 휴가 신청 내역 조회
+    // 휴가 구분 조회
+
+    // paging
+
 }
