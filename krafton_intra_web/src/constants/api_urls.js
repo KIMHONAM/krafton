76842bd4e177884433d07adf23dd5b18241cb6a9
KIM HONAM <khn228@naver.com>
2021-07-03 20:22:44 +0900

    api url 추가

--- krafton_intra_web/src/constants/api_urls.js
+++ krafton_intra_web/src/constants/api_urls.js
@@ -1,11 +1,13 @@
-const API_URL = "http://localhost:8888"
+const API_URL = 'http://localhost:8888'
 
-const GET_PTO_INFO = API_URL+"/pto/info/id/{id}"
-const GET_PTO_TYPE = API_URL+"/pto/types"
+const GET_PTO_INFO = API_URL+'/pto/info/id/{id}'
+const GET_PTO_TYPE = API_URL+'/pto/types'
+const POST_PTO = API_URL+'/pto'
 
 export const API_URLS = {
   'GET_PTO_INFO': GET_PTO_INFO,
-  'GET_PTO_TYPE': GET_PTO_TYPE
+  'GET_PTO_TYPE': GET_PTO_TYPE,
+  'POST_PTO': POST_PTO,
 }
 
 export default API_URLS
