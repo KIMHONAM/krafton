4be1d29fd37c1a86b78c5eca3b4b92e1586ca5ba
KIM HONAM <khn228@naver.com>
2021-07-03 11:14:35 +0900

    휴가 구분 조회 추가

--- krafton_intra_web/src/constants/api_urls.js
+++ krafton_intra_web/src/constants/api_urls.js
@@ -1,9 +1,11 @@
 const API_URL = "http://localhost:8888"
 
 const GET_PTO_INFO = API_URL+"/pto/info/id/{id}"
+const GET_PTO_TYPE = API_URL+"/pto/types"
 
 export const API_URLS = {
   'GET_PTO_INFO': GET_PTO_INFO,
+  'GET_PTO_TYPE': GET_PTO_TYPE
 }
 
 export default API_URLS
