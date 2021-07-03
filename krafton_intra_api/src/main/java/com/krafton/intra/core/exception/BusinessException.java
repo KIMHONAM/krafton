33d90e2ed372519a839e30013bee58ffdde2dcd5
KIM HONAM <khn228@naver.com>
2021-07-03 20:19:42 +0900

    에러 처리 세팅 수정

--- krafton_intra_api/src/main/java/com/krafton/intra/core/exception/BusinessException.java
+++ krafton_intra_api/src/main/java/com/krafton/intra/core/exception/BusinessException.java
@@ -14,16 +14,19 @@ public class BusinessException extends RuntimeException{
     }
     public BusinessException(String errorCode) {
         super(errorCode);
+        this.errorCode = errorCode;
     }
 
 
     public BusinessException(String errorCode, String errorMessage) {
         super(errorCode);
+        this.errorCode = errorCode;
         this.errorMessage = errorMessage;
     }
 
     public BusinessException(String errorCode, String errorMessage, Throwable t) {
         super(errorCode, t);
+        this.errorCode = errorCode;
         this.errorMessage = errorMessage;
     }
 
