e5d5410dd5ac0d042c50d04027af0e458774c6de
KIM HONAM <khn228@naver.com>
2021-07-03 20:20:55 +0900

    데이터 변경

--- krafton_intra_api/src/main/resources/data.sql
+++ krafton_intra_api/src/main/resources/data.sql
@@ -8,7 +8,7 @@ INSERT INTO employee (employee_number,department_code,name,position_id, hire_dat
 
 INSERT INTO employee_pto_summary (employee_id, occur_days, use_days, unused_days, create_user) values (1,15.0,3.5,11.5,1);
 
-INSERT INTO employee_pto_history (employee_id, start_date, end_date, reason, status, pto_type, pto_days, applicate_date, applicant, approver, approve_date, create_user) values(1,'2021-06-04 00:00:00','2021-06-05 00:00:00','개인 사유', 'PTOS000001', 'PTOT000001', 2, '2021-06-02 00:00:00', 1,1,'2021-06-02 00:00:00',1);
+INSERT INTO employee_pto_history (id,employee_id, start_date, end_date, reason, status, pto_type, pto_days, applicate_date, applicant, approver, approve_date, create_user) values('1_20210703184433',1,'2021-06-04 00:00:00','2021-06-05 00:00:00','개인 사유', 'PTOS000001', 'PTOT000001', 2, '2021-06-02 00:00:00', 1,1,'2021-06-02 00:00:00',1);
 
 INSERT INTO common_code (code,code_name,sort_order,create_user) values ('PTOS000001','신청',1,1);
 INSERT INTO common_code (code,code_name,sort_order,create_user) values ('PTOS000002','취소',2,1);
